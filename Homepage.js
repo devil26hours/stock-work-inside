a58b1689d777ef5a781a323a1d194487d61bda0c
devil26hours <taego_Good@hotmail.com>
2022-11-16 20:14:40 +0700

    Login!

--- Homepage.js
+++
@@ -1,9 +0,0 @@
-// var jwt = localStorage.getItem("jwt")
-// if (jwt == null){
-//     window.location.href ='./login.html'
-// }
-
-// function logout() {
-//     localStorage.removeItem("jwt");
-//     window.location.href = "./login.html";
-// }
