80342769e3441c67dc71c2f74edd52d10ba14f46
devil26hours <taego_Good@hotmail.com>
2022-11-16 13:04:00 +0700

    13.03

--- login.js
+++ login.js
@@ -1,46 +1,75 @@
-// var jwt = localStorage.getItem("jwt")
-// if (jwt != null){
-//     window.location.href ='./HomePage.html'
-// }
+const mysql = require('mysql');
+const express = require('express');
+const session = require('express-session');
+const path = require('path');
 
-// function login() {
-//     const username = document.getElementById("username").value;
-//     const password = document.getElementById("password").value;
+const connection = mysql.createConnection({
+	host     : 'localhost',
+	user     : 'root',
+	password : '',
+	database : 'nodelogin'
+});
 
-//     const xhttp = new XMLHttpRequest();
-//     xhttp.open("POST", "https://www.mecallapi.com/api/login");
-//     xhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-//     xhttp.send(JSON.stringify({
-//         "username" : username,
-//         "password" : password
-//     }));
-//     xhttp.onreadystatechange = function () {
-//         if (this.readyState == 4) {
-//             const object = JSON.parse(this.responseText);
-//             console.log(object);
-//             if(object['status'] == 'ok') {
-//                 localStorage.setItem("jwt", object["accessToken"]);
-//                 Swal.fire({
-//                     position: 'center',
-//                     icon: 'success',
-//                     title: 'Login success!',
-//                     showConfirmButton: false,
-//                     timer: 1500 
-//                   })
-//                   window.location.href ='./HomePage.html'
-//             } else {
-//                 Swal.fire(
-//                     object['message'],
-//                     'Wrong Email or Password?',
-//                     'error'
-//                   )
-//             }
-//         }
-//     }
-//     return false;
-// }
+const app = express();
 
-// function logout() {
-//     localStorage.removeItem("jwt");
-//     window.location.href = "./login.html";
-// }
+app.use(session({
+	secret: 'secret',
+	resave: true,
+	saveUninitialized: true
+}));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'static')));
+
+
+// http://localhost:3000/
+app.get('/', function(request, response) {
+	// Render login template
+	response.sendFile(path.join(__dirname + '/login.html'));
+});
+
+
+// http://localhost:3000/auth
+app.post('/auth', function(request, response) {
+	// Capture the input fields
+	let username = request.body.username;
+	let password = request.body.password;
+	// Ensure the input fields exists and are not empty
+	if (username && password) {
+		// Execute SQL query that'll select the account from the database based on the specified username and password
+		connection.query('SELECT * FROM accounts WHERE username = ? AND password = ?', [username, password], function(error, results, fields) {
+			// If there is an issue with the query, output the error
+			if (error) throw error;
+			// If the account exists
+			if (results.length > 0) {
+				// Authenticate the user
+				request.session.loggedin = true;
+				request.session.username = username;
+				// Redirect to home page
+				response.redirect('/home');
+			} else {
+				response.send('Incorrect Username and/or Password!');
+			}			
+			response.end();
+		});
+	} else {
+		response.send('Please enter Username and Password!');
+		response.end();
+	}
+});
+
+// http://localhost:3000/home
+app.get('/home', function(request, response) {
+	// If the user is loggedin
+	if (request.session.loggedin) {
+		// Output username
+		return response.sendFile(path.join(__dirname + '/home.html'));
+	} else {
+		// Not logged in
+		response.send('Please login to view this page!');
+	}
+	response.end();
+});
+
+app.listen(3000),
+console.log('server start on port 3000!');
--- server.js
+++ server.js
@@ -13,19 +13,22 @@ const jwt = require('jsonwebtoken');
 const Connection = require('mysql2');
 const { response, request } = require('express');
 const secret = 'shippop-stock-login'
-
-
+const path = require('path');
+const session = require('express-session');
 
 
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true }));
 
+app.use(session({
+	secret: 'secret',
+	resave: true,
+	saveUninitialized: true
+}));
 
 
-const server = app.listen(3000,()=> {
-    console.log ('nodejs is running on port 3000!')
-})
+
 
 const db = mysql.createConnection({
   host: "localhost",
@@ -38,6 +41,62 @@ db.connect(function(err) {
   if (err) throw err;
   console.log("Connected!");
 });
+
+// http://localhost:3000/
+app.get('/', function(request, response) {
+	// Render login template
+	response.sendFile(path.join(__dirname + '/login.html'));
+});
+
+
+// http://localhost:3000/auth
+app.post('/auth', function(request, response) {
+	// Capture the input fields
+	let username = request.body.username;
+	let password = request.body.password;
+	// Ensure the input fields exists and are not empty
+	if (username && password) {
+		// Execute SQL query that'll select the account from the database based on the specified username and password
+		db.query('SELECT * FROM accounts WHERE username = ? AND password = ?', [username, password], function(error, results, fields) {
+			// If there is an issue with the query, output the error
+			if (error) throw error;
+			// If the account exists
+			if (results.length > 0) {
+				// Authenticate the user
+				request.session.loggedin = true;
+				request.session.username = username;
+				// Redirect to home page
+				response.redirect('/home');
+			} else {
+				response.send('Incorrect Username and/or Password!');
+			}			
+			response.end();
+		});
+	} else {
+		response.send('Please enter Username and Password!');
+		response.end();
+	}
+});
+
+// http://localhost:3000/home
+app.get('/home', function(request, response) {
+	// If the user is loggedin
+	if (request.session.loggedin) {
+		// Output username
+		return response.sendFile(path.join(__dirname + '/HomePage.html'));
+	} else {
+		// Not logged in
+		response.send('Please login to view this page!');
+	}
+	response.end();
+});
+
+
+const server = app.listen(3000,()=> {
+    console.log ('nodejs is running on port 3000!')
+})
+
+
 
 //create
 app.post('/api/createstock', (req, res)=>{
